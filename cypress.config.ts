d351c8e67c09f524fcda5ee7f0799e6adfd5e61b
Drake <drake@overpoweredtecchnology.com>
2026-06-09 07:31:11 -0600

    fix(cypress): update webServer command to use --strictPort for Cypress preview

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     video: false,
     supportFile: false,
     webServer: {
-      command: 'npm run preview -- --host 127.0.0.1 --port 4173',
+      command: 'npm run preview -- --host 127.0.0.1 --port 4173 --strictPort',
       url: 'http://127.0.0.1:4173',
       reuseExistingServer: !process.env.CI,
     },
